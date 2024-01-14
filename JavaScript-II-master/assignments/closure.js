a88e7fcea54d88468bcfcccd709595c1e4519e5e
millan <millan.fig@gmail.com>
2024-01-13 17:17:22 -0800

    Completed closure and callbacks exercises.

--- JavaScript-II-master/assignments/closure.js
+++ JavaScript-II-master/assignments/closure.js
@@ -1,20 +1,51 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function simpleClosure(){
 
+  return () => {
+    return 'Well Hello'
+  }
+}
+
+const simple = simpleClosure();
+console.log(simple());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let num = 0;
+  return () => {
+    num += 1;
+    console.log(num);
+  };
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+function counterFactory(){
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let num = 0;
+  this.increment = function(){ 
+    return () => {
+      num += 1;
+      console.log(num);
+    };
+  },
+  this.decrement = function(){ 
+    return () => {
+      num -= 1;
+      console.log(num);
+    };
+  }
 };
+
+const counter2 = new counterFactory();
+counter2.increment();
+counter2.decrement();
